17d317847cc3275409269f0fcaaf49c5e22c36af
Ace <ace.github@gmail.com>
2014-08-15 17:21:05 -0700

    Added rough draft of script for pulling Github user information

---
+++ other/gitinfo.py
@@ -0,0 +1,18 @@
+# Script for pulling information about 
+
+from bs4 import BeautifulSoup
+import urllib.request
+
+class User:
+	def __init__(self, username):
+		self.username = username
+		self.url = urllib.request.urlopen('https://www.github.com/{user}'.format(user=self.username))
+		self.soup = BeautifulSoup(self.url)
+		self.body = self.soup.get_text()
+
+	def commit_streak(self):
+		return self.soup.find_all('span', {'class':'num'})
+
+if __name__ == '__main__':
+	AceCodes = User('acecodes') # Using my username as a test
+	print(AceCodes.commit_streak())
