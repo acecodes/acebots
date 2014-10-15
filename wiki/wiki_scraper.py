157d0e13d5ddcadb37a41d3c5a809f34c3fce1d2
Ace <ace.github@gmail.com>
2014-10-14 21:18:08 -0700

    URL changed, renamed classes, added comments

--- wiki/wiki_scraper.py
+++ wiki/wiki_scraper.py
@@ -5,7 +5,7 @@ from datetime import datetime, date
 from re import findall, sub
 from bs4 import BeautifulSoup
 
-class ScrapeSite:
+class WikiScrape:
 
 	def __init__(self, url):
 		self.header = {'User-Agent': 'Mozilla/5.0 (X11; Linux x86_64) AppleWebKit/537.11 (KHTML, like Gecko) Chrome/23.0.1271.64 Safari/537.11',
@@ -14,18 +14,23 @@ class ScrapeSite:
        'Accept-Encoding': 'none',
        'Accept-Language': 'en-US,en;q=0.8',
        'Connection': 'keep-alive'}
-		self.url = url
+		self.url = "http://en.wikipedia.org/wiki/" + url
 		self.req = urllib.request.Request(self.url, headers=self.header)
 		self.data = urllib.request.urlopen(self.req)
 		self.soup = BeautifulSoup(self.data)
 		self.body = self.soup.get_text()
 		self.infoboxes = self.soup.find_all('table', {"class":"infobox"})
+	
+	# Pull images for each potential flashcard	
+	def pull_images(self):
+		pass
 
 	def regex(self, string):
 		return findall(string, self.body)
 
 
-Neuron = ScrapeSite('http://en.wikipedia.org/wiki/Neuron')
+Neuron = WikiScrape('Neuron')
 
 if __name__ == '__main__':
+	# Test with Neuron Wikipedia page
 	print(Neuron.infoboxes)
