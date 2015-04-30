36a95d8e3204a82fafb5ac1ce37cfad0237cf95d
Ace <ace.github@gmail.com>
2015-04-29 17:49:34 -0700

    Cleaned up trend outputs, added intersection of global and US trends

--- twitter/acescan.py
+++ twitter/acescan.py
@@ -1,5 +1,6 @@
 import twitter
 import os
+import json
 
 """
 Code used in conjunction with
@@ -23,5 +24,12 @@ US_WOE_ID = 23424977
 world_trends = twitter_api.trends.place(_id=WORLD_WOE_ID)
 us_trends = twitter_api.trends.place(_id=US_WOE_ID)
 
-for trends in world_trends['trends']:
-    print(trends['name'])
+print(json.dumps(world_trends, indent=1))
+print(json.dumps(us_trends, indent=1))
+
+world_trends_set = set([trend['name'] for trend in world_trends[0]['trends']])
+us_trends_set = set([trend['name'] for trend in us_trends[0]['trends']])
+
+common_trends = world_trends_set.intersection(us_trends_set)
+
+print(common_trends)
