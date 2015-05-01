4a559693a8234561a6b52426df45747e8a5e3950
Ace <ace.github@gmail.com>
2015-05-01 13:08:45 -0700

    Frequency distributions added for Twitter

--- twitter/search_results.py
+++ twitter/search_results.py
@@ -1,6 +1,8 @@
 import twitter
 import os
 import json
+from collections import Counter
+from prettytable import PrettyTable
 
 """Nope!"""
 CONSUMER_KEY = os.environ.get('TWIT_CON_KEY')
@@ -21,15 +23,41 @@ search_results = twitter_api.search.tweets(q=query, count=count)
 
 statuses = search_results['statuses']
 
-for _ in range(5):
-    print('Length of statuses', len(statuses))
-    try:
-        next_results = search_results['search_metadata']['next_results']
-    except KeyError:
-        break
+if __name__ == '__main__':
+    # for _ in range(5):
+    #     print('Length of statuses', len(statuses))
+    #     try:
+    #         next_results = search_results['search_metadata']['next_results']
+    #     except KeyError:
+    #         break
 
-    kwargs = dict([kv.split('=') for kv in next_results[1:].split('&')])
-    search_results = twitter_api.search.tweets(**kwargs)
-    statuses += search_results['statuses']
+    #     kwargs = dict([kv.split('=') for kv in next_results[1:].split('&')])
+    #     search_results = twitter_api.search.tweets(**kwargs)
+    #     statuses += search_results['statuses']
 
-print(json.dumps(statuses[0], indent=1))
+    # print(json.dumps(statuses[0], indent=1))
+    status_texts = [status['text'] for status in statuses]
+
+    screen_names = [user_mention['screen_name'] for status in statuses for user_mention in status['entities']['user_mentions']]
+
+    # Next two are lower-case to normalize the results
+    hash_tags = [hashtag['text'].lower() for status in statuses for hashtag in status['entities']['hashtags']]
+    words = [w.lower() for t in status_texts for w in t.split()]
+
+    # print('Sample data sets:')
+    # print(json.dumps(status_texts[0:5], indent=1))
+    # print(json.dumps(screen_names[0:5], indent=1))
+    # print(json.dumps(hash_tags[0:5], indent=1))
+    # print(json.dumps(words[0:5], indent=1))
+    
+    print('\nFrequency distributions:')
+
+    for label, data in (('Word', words),
+                        ('Screen Name', screen_names),
+                        ('Hash tag', hash_tags)):
+        pt = PrettyTable(field_names=[label, 'Count'])
+        c = Counter(data)
+        [pt.add_row(kv) for kv in c.most_common()[:10]]
+        pt.align[label], pt.align['Count'] = 'l', 'r' # Column alignment
+        print(pt)
+
